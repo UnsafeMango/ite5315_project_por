57b2a533d627e6ab83b670cf219de2b18d01fb8d
unsafemango <unsafemango@gmail.com>
2022-11-26 21:12:06 -0500

    added 5 functions

--- config/database.js
+++ config/database.js
@@ -1,23 +1,30 @@
 var mongoose = require("mongoose");
 
-var restaurants = require("../models/restaurant");
+var restaurant = require("../models/restaurant");
 
 module.exports = {
   // establish connection to database
-  initialize: function (url) {
+  initialize(url) {
     if (mongoose.connect(url)) {
+      console.log("====================================");
+      console.log("Connected to the database...");
+      console.log("====================================");
       return true;
     } else {
-      console.log("not connected...");
+      console.log("Error! Could not connect to the database...");
       return false;
     }
   },
-  // add new restaurant to database
-  addNewRestaurant: function (data) {
-    restaurant.create({}, function (err, data) {
-      if (err) throw err;
 
+  // add new restaurant to database using the data object
+  addNewRestaurant(data) {
+    restaurant.create(data, (err, data) => {
+      if (err) throw err;
+      console.log("====================================");
+      console.log("Successful! Document inserted...");
+      console.log("====================================");
       let id = data._id;
+
       // get the newly created restaurant from database
       restaurant.findById(id, (err, data) => {
         if (err) throw err;
@@ -25,6 +32,43 @@ module.exports = {
         console.log(data);
         console.log("====================================");
       });
+    });
+  },
+
+  // get all restaurants from database
+  getAllRestaurants(page, perPage, borough) {},
+
+  // get a restaurant from database
+  getRestaurantById(id) {
+    let document = "";
+    restaurant.findById(id, (err, data) => {
+      if (err) throw err;
+      document = data;
+    });
+
+    return document;
+  },
+
+  // update a restaurant in the database
+  updateRestaurantById(data, id) {
+    // save the restaurant to the database...
+    restaurant.findByIdAndUpdate(id, data, (err, data) => {
+      if (err) throw err;
+      console.log("====================================");
+      console.log(`Successful! Restaurant updated - ${data.name}`);
+      console.log("====================================");
+    });
+  },
+
+  // delete a restaurant from database
+  deleteRestaurant(id) {
+    restaurant.remove({ _id: id }, (err) => {
+      if (err) throw err;
+      else {
+        console.log("====================================");
+        console.log("Successful! Restaurant has been deleted...");
+        console.log("====================================");
+      }
     });
   },
 };
